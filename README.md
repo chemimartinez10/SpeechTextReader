aa32859a379754606c1cf50cb0a1b4210cfa9772
Chemi Martinez <jmmartineztuarez@gmail.com>
2020-10-08 10:16:57 -0400

    All done
    Scripts and previews

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SpeechTextReader
  A gallery of photos with and speech reader
 ![preview](preview.png)
+![preview2](preview2.png)
---
+++ script.js
@@ -0,0 +1,133 @@
+const main = document.querySelector('main')
+const voicesSelect = document.getElementById('voices')
+const textArea = document.getElementById('text')
+const read = document.getElementById('read')
+const toggleBtn = document.getElementById('toggle')
+const closeBtn = document.getElementById('close')
+
+const data = [{
+        image: './img/drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: './img/food.jpg',
+        text: "I'm Hungry"
+    },
+    {
+        image: './img/tired.jpg',
+        text: "I'm Tired"
+    },
+    {
+        image: './img/hurt.jpg',
+        text: "I'm Hurt"
+    },
+    {
+        image: './img/happy.jpg',
+        text: "I'm Happy"
+    },
+    {
+        image: './img/angry.jpg',
+        text: "I'm Angry"
+    },
+    {
+        image: './img/sad.jpg',
+        text: "I'm Sad"
+    },
+    {
+        image: './img/scared.jpg',
+        text: "I'm Scared"
+    },
+    {
+        image: './img/outside.jpg',
+        text: 'I Want To Go Outside'
+    },
+    {
+        image: './img/home.jpg',
+        text: 'I Want To Go Home'
+    },
+    {
+        image: './img/school.jpg',
+        text: 'I Want To Go To School'
+    },
+    {
+        image: './img/grandma.jpg',
+        text: 'I Want To Go To Grandmas'
+    }
+];
+
+let voices = []
+
+//init speech synth
+
+const message = new SpeechSynthesisUtterance()
+
+data.forEach(createBox)
+
+function createBox(item) {
+    const box = document.createElement('div')
+    const {
+        image,
+        text
+    } = item
+    box.classList.add('box')
+    box.innerHTML = `
+        <img src="${image}" alt="${text}"/>
+        <p class="info">${text}</p>
+    `
+
+    box.addEventListener('click', () => {
+        setTextMessage(text)
+        speechMessage()
+        box.classList.add('active')
+        setTimeout(() => box.classList.remove('active'), 800)
+    })
+
+    main.appendChild(box)
+}
+
+function getVoices() {
+    voices = speechSynthesis.getVoices()
+    voices.forEach((voice) => {
+        const option = document.createElement('option')
+        option.value = voice.name
+        option.innerText = `${voice.name} ${voice.lang}`
+
+        voicesSelect.appendChild(option)
+
+    })
+}
+
+function setTextMessage(text) {
+    message.text = text
+}
+
+function speechMessage() {
+    speechSynthesis.speak(message)
+}
+
+function setVoice(e) {
+    // const selectedVoice = e.target.value
+    // voices.forEach((voice) => {
+    //     if (voice.name === selectedVoice) {
+    //         message.voice = voice
+    //     }
+
+    // })
+
+    message.voice = voices.find(voice => voice.name === e.target.value)
+
+}
+
+
+
+speechSynthesis.addEventListener('voiceschanged', getVoices)
+toggleBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'))
+closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'))
+voicesSelect.addEventListener('change', setVoice)
+
+read.addEventListener('click', () => {
+    setTextMessage(textArea.value)
+    speechMessage()
+})
+
+getVoices();
